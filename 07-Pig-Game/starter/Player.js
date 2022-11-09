a87e973fb5b47a25f0c4f98d3a4048e24d563b71
Kalel Poeta <kalel.poeta@orbisag.ch>
2022-11-09 09:32:10 +0100

    Added all functionalities besides new game

---
+++ 07-Pig-Game/starter/Player.js
@@ -0,0 +1,10 @@
+class Player {
+  constructor(name, current, score, currentLabel, scoreLabel, section) {
+    this.name = name;
+    this.current = current;
+    this.score = score;
+    this.currentLabel = currentLabel;
+    this.scoreLabel = scoreLabel;
+    this.section = section;
+  }
+}
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,61 @@
-'use strict';
+"use strict";
+
+const dice = document.querySelector(".dice");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+let playerOneTurn = true;
+let playerOne = new Player(
+  "playerOne",
+  0,
+  0,
+  document.querySelector("#current--0"),
+  document.querySelector("#score--0"),
+  document.querySelector(".player--0")
+);
+let playerTwo = new Player(
+  "playerTwo",
+  0,
+  0,
+  document.querySelector("#current--1"),
+  document.querySelector("#score--1"),
+  document.querySelector(".player--1")
+);
+const activatePlayerSection = function (activePlayer, deactivePlayer) {
+  activePlayer.section.classList.add("player--active");
+  deactivePlayer.section.classList.remove("player--active");
+};
+const changePlayers = function (deactivePlayer) {
+  deactivePlayer.current = 0;
+  deactivePlayer.currentLabel.textContent = deactivePlayer.current;
+  playerOneTurn = !playerOneTurn;
+  playerOneTurn
+    ? activatePlayerSection(playerOne, playerTwo)
+    : activatePlayerSection(playerTwo, playerOne);
+};
+
+btnRoll.addEventListener("click", () => {
+  playerOneTurn ? rollDice(playerOne) : rollDice(playerTwo);
+});
+
+btnHold.addEventListener("click", () => {
+  playerOneTurn ? holdCurrentScore(playerOne) : holdCurrentScore(playerTwo);
+});
+
+function rollDice(player) {
+  let diceFace = Math.trunc(Math.random() * 6) + 1;
+  dice.setAttribute("src", `dice-${diceFace}.png`);
+
+  if (diceFace !== 1) {
+    player.current += diceFace;
+  } else {
+    changePlayers(player);
+  }
+  player.currentLabel.textContent = player.current;
+}
+
+function holdCurrentScore(player) {
+  player.score += player.current;
+  player.scoreLabel.textContent = player.score;
+  changePlayers(player);
+  console.log(playerOneTurn);
+}
